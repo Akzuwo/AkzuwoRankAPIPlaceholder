7e01f4dc0a0e47ca00b9a2dde6fe54df96041ae5
Akzuwo <157422136+Akzuwo@users.noreply.github.com>
2025-06-17 14:47:51 +0200

    Switch to Velocity and provide PAPIProxyBridge placeholder

--- src/main/java/ch/ksrminecraft/akzuwoRankAPIPlaceholder/utils/RankAPI.java
+++ src/main/java/ch/ksrminecraft/akzuwoRankAPIPlaceholder/utils/RankAPI.java
@@ -1,18 +1,44 @@
 package ch.ksrminecraft.akzuwoRankAPIPlaceholder.utils;
 
 import ch.ksrminecraft.RankPointsAPI.PointsAPI;
+import org.slf4j.Logger;
+import org.yaml.snakeyaml.Yaml;
 
-import java.util.logging.Logger;
-
-import static javax.sql.rowset.spi.SyncFactory.getLogger;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.UUID;
 
 public class RankAPI {
-    Logger logger = getLogger(); // or any other logger
-    PointsAPI api = new PointsAPI(
-            "jdbc:mysql://host:port/database",
-            "username",
-            "password",
-            logger,
-            true // enable debug
-    );
+    private final PointsAPI api;
+
+    public RankAPI(Path dataDirectory, Logger logger) throws Exception {
+        Path configFile = dataDirectory.resolve("config.yml");
+        if (Files.notExists(configFile)) {
+            try (InputStream in = RankAPI.class.getClassLoader().getResourceAsStream("config.yml")) {
+                if (in != null) {
+                    Files.copy(in, configFile);
+                }
+            }
+        }
+
+        Yaml yaml = new Yaml();
+        Map<String, Object> config;
+        try (InputStream in = Files.newInputStream(configFile)) {
+            config = yaml.load(in);
+        }
+
+        Map<String, Object> db = (Map<String, Object>) config.get("database");
+        String url = (String) db.get("url");
+        String username = (String) db.get("username");
+        String password = (String) db.get("password");
+        boolean debug = Boolean.parseBoolean(String.valueOf(db.getOrDefault("debug", false)));
+
+        this.api = new PointsAPI(url, username, password, logger, debug);
+    }
+
+    public int getPoints(UUID uuid) {
+        return api.getPoints(uuid);
+    }
 }
